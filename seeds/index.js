22a7ad87057a3eda2e74fecf0eca30f4960f4243
Ryan Safaeian <ryansafa2@gmail.com>
2021-08-02 21:39:28 -0700

    added styling

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const randomNumber = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur placeat similique libero, consectetur alias at doloremque facilis possimus excepturi quos, architecto incidunt eaque labore neque, ullam quam minima recusandae sunt.',
+            price
         });
         await camp.save();
     }
