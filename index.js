2e1d516e4dcac80b021b297acca9e0daa8ce1d1a
RyanSafa <ryansafa2@gmail.com>
2021-08-07 22:53:50 -0500

    reviews validation

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const path = require('path');
 const ejsMate = require('ejs-mate')
-const  { campgroundSchema } = require('./schemas.js')
+const  { campgroundSchema, reviewSchema } = require('./schemas.js')
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync')
 const Campground = require('./models/campground');
 const { join } = require('path');
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -33,6 +34,17 @@ app.use(methodOverride('_method'));
 
 const validateCampground = (req,res,next) =>{
     const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map((el) =>el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+const validateReviews = (req,res,next) =>{
+    console.log(reviewSchema)
+    const {error} = reviewSchema.validate(req.body);
     if(error){
         const msg = error.details.map((el) =>el.message).join(',');
         throw new ExpressError(msg, 400);
@@ -84,6 +96,15 @@ app.delete('/campgrounds/:id', catchAsync(async(req,res ) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+app.post('/campgrounds/:id/reviews', validateReviews, catchAsync(async(req,res) =>{
+   const campground =  await Campground.findById(req.params.id);
+   const review = new Review(req.body.review)
+   campground.reviews.push(review);
+   await review.save();
+   await campground.save();
+   res.redirect(`/campgrounds/${campground._id}`)
 }));
 
 app.all('*', (req,res,next) => {
